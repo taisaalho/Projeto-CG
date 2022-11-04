8be041436f549f79c8fbcf29cad6f7901fb35930
taisaalho <92999297+taisaalho@users.noreply.github.com>
2022-11-04 12:50:57 +0000

    Sprite personagem

---
+++ Teste de Animação/animaçãoSprite.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector('#myCanvas');
+const ctx = canvas.getContext("2d");
+
+const W = canvas.width, H = canvas.height;
+
+let imageIDLE = new Image();
+imageIDLE.src = '../Packs/Monsters/Pink_Monster/Pink_Monster_Idle_4.png'
+
+let imageUP = new Image();
+imageUP.src = '../Packs/Monsters/Pink_Monster/Pink_Monster_Climb_4.png'
+
+let imageWalkRight = new Image();
+imageWalkRight.src = '../Packs/Monsters/Pink_Monster/Pink_Monster_Walk_6.png'
+
+let image;
+
+window.onload = function () {
+	setInterval(render, 2000 / 15); //start animation AFTER image load! - 15 fps
+	image = imageIDLE;
+};
+
+//sprite frame counter 		
+let frameIndex = 0;
+
+function render() {
+ctx.clearRect(0, 0, W, H);
+
+ctx.drawImage(image, frameIndex * 32, 0, 32, 32,
+    110, 80, 100, 100);
+
+
+frameIndex++;
+if (frameIndex == 4)
+	frameIndex = 0; //reset the number of frames counter
+
+}
+
+//TECLADO
+window.addEventListener("keydown", keypressed);
+
+function keypressed(click){
+	console.log(click.key)
+	if (click.key == "w"){
+		image = imageUP
+	}else if (click.key == "s"){
+		image = imageIDLE
+	}else if (click.key == "d"){
+		image = imageWalkRight
+	}
+}
