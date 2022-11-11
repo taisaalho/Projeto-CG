1d64141e63e7b943649ff1c761d054ce996e39db
taisaalho <92999297+taisaalho@users.noreply.github.com>
2022-11-11 13:01:19 +0000

    Animação Personagem 1 Completa

--- Teste de Animação/animaçãoSprite.js
+++ Teste de Animação/animaçãoSprite.js
@@ -1,10 +1,10 @@
 const canvas = document.querySelector('#myCanvas');
 const ctx = canvas.getContext("2d");
 
-const W = canvas.width, H = canvas.height;
+const W = canvas.width = 500 , H = canvas.height = 500;
 
 
-//Imagens
+/* //Imagens
 let imageIdle = new Image();
 imageIdle.src = '../Packs/Monsters/Pink_Monster/Pink_Monster_Idle_4.png'
 
@@ -68,4 +68,83 @@ function keyReleased(click){
 		image = imageIdle
 	}else if (click.key == "s" || click.key == "S")
 		image = imageIdle
+} */
+
+
+
+
+//Images
+let imageFront = new Image();
+imageFront.src = 'Sprite_Menina_Front.png'
+
+let imageBack = new Image();
+imageBack.src = 'Sprite_Menina_Back.png'
+
+let imageWalkRight = new Image();
+imageWalkRight.src = 'Sprite_Menina_Right.png'
+
+let imageWalkLeft = new Image();
+imageWalkLeft.src = 'Sprite_Menina_Left.png'
+
+let imageStop = new Image();
+imageStop.src = 'Sprite_Menina_Stop.png'
+
+
+let image;
+
+window.onload = function () {
+	setInterval(render, 2050 / 15); 
+	image = imageStop
+};
+
+
+//Sprite frame counter 		
+let frameIndex = 0;
+
+function render() {
+ctx.clearRect(0, 0, W, H);
+
+//ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh) 
+/* sx - Onde começa a imagem (x)
+sy - Onde começa a imagem (y) 
+sw - Tamanho da imagem (x)
+sh - Tamanho da imagem (y)
+dx - Localização da imagem (x)
+dy - Localização da imagem (y)
+dw - */
+ctx.drawImage(image,frameIndex * 64, 0, 64, 64,
+    0, 0, 100, 100);
+
+frameIndex++;
+if (frameIndex == 4)
+	frameIndex = 0;
+
 }
+
+//Teclado
+window.addEventListener("keydown", keyPressed);
+
+function keyPressed(click){
+	console.log(click.key)
+	if (click.key == "w" || click.key == "W"){
+		image = imageBack
+	}else if (click.key == "d" || click.key == "D"){
+		image = imageWalkRight
+	}else if (click.key == "a" || click.key == "A"){
+		image = imageWalkLeft
+	}else if (click.key == "s" || click.key == "S")
+		image = imageFront 
+}
+
+window.addEventListener('keyup',keyReleased);
+
+function keyReleased(click){
+	if(click.key == "w" || click.key =="W"){
+		image = imageStop
+	}else if (click.key == "d" || click.key == "D"){
+		image = imageStop
+	}else if (click.key == "a" || click.key == "A"){
+		image = imageStop
+	}else if (click.key == "s" || click.key == "S")
+		image = imageStop
+} 
