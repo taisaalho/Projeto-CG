bb49bfc97216ee6af09c8148ba9bd0fef442dde7
ptnoob <72983038+ptnoob@users.noreply.github.com>
2022-11-22 17:03:13 +0000

    Menu inicial

--- Code/JS/menu.js
+++ Code/JS/menu.js
@@ -1,7 +1,21 @@
-const canvas = document.querySelector('#idCanvas');
-const ctx = canvas.getContext("2d");
+function inputname(){
+    document.getElementById('butoes').innerHTML = `<input type = "text" id = "nome" placeholder = "Escolhe o teu nome!"></input>
+    <button id = "submit" onclick = submit() >Confirmar</button>
+    <button id = "back" onclick = back()>Sair</button>`;
+}
 
-canvas.width = 1400;
-canvas.height = 800;
-let W = canvas.width;
-let H = canvas.height;
+function back(){
+    document.getElementById('butoes').innerHTML = `<button class="btn1" onclick=inputname()>Novo Jogo</button>
+    <button class="btn2">Sair</button>`
+}
+
+function tutorial(){
+    document.getElementById('butoes').innerHTML = `<h3 id = "move">W/A/S/D - Para mover</h3>
+    <h3 id = "inte">E - Para interagir</h3>`;
+}
+
+function submit(){
+    let nome = document.getElementById('nome').value
+    window.location.href='game.html'
+    console.log(nome)
+}
